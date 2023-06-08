6ac7a4021d917eb554f5a92039a9175fc2f0b1d3
G2305635 <G2305635@HMC-UW-D042185.hkmc.hkmg.global>
2023-06-08 13:22:17 +0900

    test update

--- MainEventFlow/src/main/java/com/autoever/poc/common/StringUtils.java
+++ MainEventFlow/src/main/java/com/autoever/poc/common/StringUtils.java
@@ -5,8 +5,6 @@ public class StringUtils {
 	public StringUtils() {}
 	
 	public static String TestString() {
-		
-		return "hoon_7R7wJgMB3Uzaf8";
 	}
 	public static byte[] mergeByteArray(byte[] first, byte[] second) {
 		byte[] combined = new byte[first.length + second.length];
