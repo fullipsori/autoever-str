ef3a39a1880465f946b62b31e014117d8c0d3a69
G2305635 <G2305635@HMC-UW-D042185.hkmc.hkmg.global>
2023-08-01 10:29:50 +0900

    fixed UniqueDatas

--- MainEventFlow/src/main/java/com/autoever/poc/common/UniqueDatas.java
+++ MainEventFlow/src/main/java/com/autoever/poc/common/UniqueDatas.java
@@ -2,6 +2,7 @@ package com.autoever.poc.common;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.IntStream;
 
 import com.streambase.sb.CompleteDataType;
 import com.streambase.sb.Tuple;
@@ -21,30 +22,24 @@ public class UniqueDatas extends AggregateWindow {
 	}
 
 	public List<?> calculate() {
-		// TODO Implement function here
 		return accDatas;
+	}
+
+	public static String getStringField(Tuple tuple, int index) {
+		try {
+			return tuple.getString(index);
+		} catch(Exception e) {
+			return null;
+		}
 	}
 
 	@CustomFunctionResolver("accumulateCustomFunctionResolver0")
 	public void accumulate(Tuple inData, boolean isFirst) {
-		// TODO Implement function here
 		if(inData == null) return;
-		try {
-			String fieldName = inData.getString(0);
-			if(fieldName == null) return;
-			Tuple tData = accDatas.stream().filter(t -> {
-				try {
-					return t.getString(0).equals(fieldName);
-				} catch (Exception e) {
-					return false;
-				}
-			}).findFirst().orElse(null);
-			if(tData == null || !isFirst) {
-				accDatas.add(inData);
-			}
-
-		}catch(Exception e) { }
-
+		String fieldName = getStringField(inData, 0);
+		if(fieldName == null) return;
+		IntStream.range(0, accDatas.size()).filter(i -> fieldName.equals(getStringField(accDatas.get(i), 0)))
+			.findFirst().ifPresentOrElse(i -> { if(!isFirst) accDatas.set(i, inData);}, () -> accDatas.add(inData));
 	}
 
 	public static CompleteDataType accumulateCustomFunctionResolver0(CompleteDataType inData,
